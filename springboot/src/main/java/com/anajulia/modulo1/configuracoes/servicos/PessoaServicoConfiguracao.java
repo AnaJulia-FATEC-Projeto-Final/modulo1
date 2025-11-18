b0c99edf7ed4199f3675d13a899ef5a98566f224
ajlima12 <ajlimaa12@gmail.com>
2025-11-18 16:44:23 -0300

    configuracao servico pessoa

---
+++ springboot/src/main/java/com/anajulia/modulo1/configuracoes/servicos/PessoaServicoConfiguracao.java
@@ -0,0 +1,17 @@
+package com.anajulia.modulo1.configuracoes.servicos;
+
+import com.anajulia.repositorios.PessoaRepositorio;
+import com.anajulia.servicos.PessoaServico;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PessoaServicoConfiguracao {
+
+    @Bean
+    public PessoaServico clientService(
+            PessoaRepositorio repository
+    ) {
+        return new PessoaServico(repository);
+    }
+}
